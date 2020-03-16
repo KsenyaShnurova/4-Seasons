79b894f9885b1de2c24b05b0c034a844a75d9b0a
KsenyaShnurova <57352369+KsenyaShnurova@users.noreply.github.com>
2020-03-16 15:12:50 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -192,7 +192,13 @@ function draw() {
 	tree(x,y+400);
 	tree(x+400,y+400);	
 
-	
+	//winter falling snow
+	if (mouseX>0 && mouseX<400 && mouseY>0 && mouseY<400){
+  		for (let i = 0; i < 150; i++){
+		snow[i].snowing();
+		}
+		//snowSound.play();
+	}
 
 	//spring flowers
 	if (mouseX>400 && mouseX<800 && mouseY>0 && mouseY<400) {
@@ -312,14 +318,6 @@ function mousePressed() {
 			r: random(10,25)
 		};
 		apple.push(newApple);
-	}
-
-	//winter falling snow
-	if (mouseX>0 && mouseX<400 && mouseY>0 && mouseY<400){
-  		for (let i = 0; i < 150; i++){
-		snow[i].snowing();
-		}
-		//snowSound.play();
 	}
 }
 
